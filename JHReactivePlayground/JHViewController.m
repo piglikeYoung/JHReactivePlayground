0299d5b519453697882c9f435293975eb6c3e88e
piglikeyoung <316046510@qq.com>
2015-09-08 18:00:56 +0800

    初始化

--- JHReactivePlayground/JHViewController.m
+++ JHReactivePlayground/JHViewController.m
@@ -8,6 +8,7 @@
 
 #import "JHViewController.h"
 #import "JHDummySignInService.h"
+#import "ReactiveCocoa.h"
 
 @interface JHViewController ()
 
@@ -17,8 +18,6 @@
 @property (weak, nonatomic) IBOutlet UILabel *invalidCreLabel;
 @property (weak, nonatomic) IBOutlet UIButton *signInBtn;
 
-@property (nonatomic, assign) BOOL usernameIsValid;
-@property (nonatomic, assign) BOOL passwordIsValid;
 @property (nonatomic, strong) JHDummySignInService *signInService;
 
 @end
@@ -28,21 +27,77 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    // 初始化控件状态
-    [self updateUIState];
-
-    self.signInService = [[JHDummySignInService alloc] init];
+    self.signInService = [JHDummySignInService new];
     
-    // 监听值的改变
-    [self.userNameTextfield addTarget:self action:@selector(usernameTextFieldChanged) forControlEvents:UIControlEventEditingChanged];
-    [self.pwdTextField addTarget:self action:@selector(passwordTextFieldChanged) forControlEvents:UIControlEventEditingChanged];
+    // 创建信号
+    RACSignal *validUsernameSignal = [self.userNameTextfield.rac_textSignal
+                                      // 类型转换
+                                      map:^id(NSString *text) {
+                                          return @([self isValidUsername:text]);
+                                      }];
+    // 创建信号
+    RACSignal *validPasswordSignal = [self.pwdTextField.rac_textSignal
+                                      map:^id(NSString *text) {
+                                          return @([self isValidPassword:text]);
+                                      }];
+    
+    // 根据条件设置背景颜色
+    RAC(self.pwdTextField, backgroundColor) = [validPasswordSignal map:^id(NSNumber *pwdValid) {
+        return [pwdValid boolValue] ? [UIColor clearColor] : [UIColor yellowColor];
+    }];
+    
+    RAC(self.userNameTextfield, backgroundColor) = [validUsernameSignal map:^id(NSNumber *userNameValid) {
+        return [userNameValid boolValue] ? [UIColor clearColor] : [UIColor yellowColor];
+    }];
+    
+    // 信号合成
+    RACSignal *signUpActiveSignal = [RACSignal
+                                     combineLatest:@[validUsernameSignal, validPasswordSignal]
+                                     reduce:^id(NSNumber*usernameValid, NSNumber *passwordValid){
+        return @([usernameValid boolValue] && [passwordValid boolValue]);
+    }];
+    
+    [signUpActiveSignal subscribeNext:^(NSNumber *signupActive) {
+        self.signInBtn.enabled = [signupActive boolValue];
+    }];
+    
+    // 按钮点击信号
+    [[[[self.signInBtn
+        rac_signalForControlEvents:UIControlEventTouchUpInside]
+       // 副作用，发送信号的时候所执行的操作
+       doNext:^(id x) {
+           self.signInBtn.enabled = NO;
+           self.invalidCreLabel.hidden = YES;
+       }]
+      // 信号转换
+      flattenMap:^id(id x) {
+          return [self signInSignal];
+      }]
+     subscribeNext:^(NSNumber *signedIn) {
+         self.signInBtn.enabled = YES;
+         BOOL success = [signedIn boolValue];
+         self.invalidCreLabel.hidden = success;
+         if (success) {
+             [self performSegueWithIdentifier:@"signInSuccess" sender:self];
+         }
+     }];
 }
 
-- (void)updateUIState {
-    self.userNameTextfield.backgroundColor = self.usernameIsValid ? [UIColor clearColor] : [UIColor yellowColor];
-    self.pwdTextField.backgroundColor = self.passwordIsValid ? [UIColor clearColor] : [UIColor yellowColor];
-    self.signInBtn.enabled = self.usernameIsValid && self.passwordIsValid;
+
+// 创建信号
+- (RACSignal *)signInSignal {
+    return [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber){
+        [self.signInService
+         signInWithUsername:self.userNameTextfield.text
+         password:self.pwdTextField.text
+         complete:^(BOOL success){
+             [subscriber sendNext:@(success)];
+             [subscriber sendCompleted];
+         }];
+        return nil;
+    }];
 }
+
 
 - (BOOL)isValidUsername:(NSString *)username {
     return username.length > 3;
@@ -52,32 +107,6 @@
     return password.length > 3;
 }
 
-- (void)usernameTextFieldChanged {
-    self.usernameIsValid = [self isValidUsername:self.userNameTextfield.text];
-    [self updateUIState];
-}
-
-- (void)passwordTextFieldChanged {
-    self.passwordIsValid = [self isValidPassword:self.pwdTextField.text];
-    [self updateUIState];
-}
-
-- (IBAction)signClick:(id)sender {
-    
-    self.signInBtn.enabled = NO;
-    self.invalidCreLabel.hidden = YES;
-    
-    // sign in
-    [self.signInService signInWithUsername:self.userNameTextfield.text
-                                  password:self.pwdTextField.text
-                                  complete:^(BOOL success) {
-                                      self.signInBtn.enabled = YES;
-                                      self.invalidCreLabel.hidden = success;
-                                      if (success) {
-                                          [self performSegueWithIdentifier:@"signInSuccess" sender:self];
-                                      }
-                                  }];
-}
 
 
 @end
---
+++ Podfile
@@ -0,0 +1,5 @@
+
+
+platform :ios, '7.0'
+
+pod 'ReactiveCocoa', '2.1.8'
